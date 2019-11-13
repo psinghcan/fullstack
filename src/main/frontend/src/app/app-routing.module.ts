32928c6f7dd0935137e727d0a9a63e893f8f165b
param sangha <psangha@exitrealty.com>
2019-11-13 16:31:26 -0500

    add routing

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListCustomersComponent} from './customer/list-customers/list-customers.component';
+import {ListProductsComponent} from './product/list-products/list-products.component';
+import {ListPurchasesComponent} from './purchase/list-purchases/list-purchases.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customers', component: ListCustomersComponent},
+  {path: 'products', component: ListProductsComponent},
+  {path: 'purchases', component: ListPurchasesComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListCustomersComponent } from './customer/list-customers/list-customers.component';
+import { SaveCustomerComponent } from './customer/save-customer/save-customer.component';
+import { SaveProductComponent } from './product/save-product/save-product.component';
+import { ListProductsComponent } from './product/list-products/list-products.component';
+import { ListPurchasesComponent } from './purchase/list-purchases/list-purchases.component';
+import { SavePurchaseComponent } from './purchase/save-purchase/save-purchase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListCustomersComponent,
+    SaveCustomerComponent,
+    SaveProductComponent,
+    ListProductsComponent,
+    ListPurchasesComponent,
+    SavePurchaseComponent
   ],
   imports: [
     BrowserModule,
