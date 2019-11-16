e449e1864079caa55d155d10940d6731155b394d
param sangha <psangha@exitrealty.com>
2019-11-16 13:56:35 -0500

    frontend adding routing/navigation

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {ListCustomersComponent} from './customer/list-customers/list-customers.component';
 import {ListProductsComponent} from './product/list-products/list-products.component';
 import {ListPurchasesComponent} from './purchase/list-purchases/list-purchases.component';
+import {HomeComponent} from './home/home.component';
 
 
 const routes: Routes = [
-  {path: 'customers', component: ListCustomersComponent},
-  {path: 'products', component: ListProductsComponent},
-  {path: 'purchases', component: ListPurchasesComponent}
+  {path: 'listCustomers', component: ListCustomersComponent},
+  {path: 'listProducts', component: ListProductsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'listPurchases', component: ListPurchasesComponent}
   ];
 
 @NgModule({
--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListCustomersComponent } from './customer/list-customers/list-customers.component';
@@ -10,6 +11,13 @@ import { SaveProductComponent } from './product/save-product/save-product.compon
 import { ListProductsComponent } from './product/list-products/list-products.component';
 import { ListPurchasesComponent } from './purchase/list-purchases/list-purchases.component';
 import { SavePurchaseComponent } from './purchase/save-purchase/save-purchase.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule} from  '@angular/material';
+import { MaterialModule} from './material.module';
+
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +32,12 @@ import { SavePurchaseComponent } from './purchase/save-purchase/save-purchase.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
